c3067fd25568a1c44bb42f77b5b86c1810056c0c
Danielle Batalla <dkbatalla@csu.fullerton.edu>
2018-11-01 16:07:17 -0700

    Update bot.py
    I did some research, and there were two options for creating a group of cuss words: We could put 'or' statements in our lines to compress them or create a class (what I put above). I haven't compiled them on my laptop yet, but you can try it on the lab computers to see if they work.

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import discord
 from discord.ext import commands
 
+#class for Cuss Words:
+#class CussWords:
+   # fuck = 'fuck'
+   # shit = 'shit'
+
 #this is the token needed to link this bot code back to the discord app
 TOKEN = 'NDk5NTAzNTc3MzUwMjc1MDcz.Dp9O5w.TJ09ddv1mW3AqPfym5aLWJlh2eg'
 #the prefix before every command this bot takes is '!'. Ex) !online, !offline
@@ -14,30 +19,30 @@ async def on_ready():
 #it would output 'No Cussing' in the chat
 @client.event
 async def on_message(message):
-    if 'fuck' in message.content.lower():
+    if 'fuck' or 'shit' in message.content.lower():
         await client.send_message(message.channel,'No Cussing!')
         await client.delete_message(message)
-    if 'shit' in message.content.lower():
-        await client.send_message(message.channel, 'No Cussing!')
-        await client.delete_message(message)
-    if 'bitch' in message.content.lower():
-        await client.send_message(message.channel, 'No Cussing!')
-        await client.delete_message(message)
-    if 'ass' in message.content.lower():
-        await client.send_message(message.channel, 'No Cussing!')
-        await client.delete_message(message)
-    if 'crap' in message.content.lower():
-        await client.send_message(message.channel, 'No Cussing!')
-        await client.delete_message(message)
-    if 'piss' in message.content.lower():
-        await client.send_message(message.channel, 'No Cussing!')
-        await client.delete_message(message)
-    if 'douche' in message.content.lower():
-        await client.send_message(message.channel, 'No Cussing!')
-        await client.delete_message(message)
-    if 'dick' in message.content.lower():
-        await client.send_message(message.channel, 'No Cussing!')
-        await client.delete_message(message)
+   # if 'shit' in message.content.lower():
+       # await client.send_message(message.channel, 'No Cussing!')
+       # await client.delete_message(message)
+  #  if 'bitch' in message.content.lower():
+      #  await client.send_message(message.channel, 'No Cussing!')
+       # await client.delete_message(message)
+  #  if 'ass' in message.content.lower():
+     #   await client.send_message(message.channel, 'No Cussing!')
+      #  await client.delete_message(message)
+  #  if 'crap' in message.content.lower():
+     #  await client.send_message(message.channel, 'No Cussing!')
+      #  await client.delete_message(message)
+  #  if 'piss' in message.content.lower():
+     #   await client.send_message(message.channel, 'No Cussing!')
+      #  await client.delete_message(message)
+  #  if 'douche' in message.content.lower():
+      #  await client.send_message(message.channel, 'No Cussing!')
+      #  await client.delete_message(message)
+  #  if 'dick' in message.content.lower():
+      #  await client.send_message(message.channel, 'No Cussing!')
+       # await client.delete_message(message)
     if '!stop' in message.content.lower():
         await client.logout()
 #another event handler.  If a user deletes a message,the bot will output
