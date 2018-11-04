ac3fa5ca827a5d5812b798f4feb4751cf8b55b3c
mesorangerxx <lqtruongnguyen@gmail.com>
2018-11-03 22:59:54 -0700

    Added coinflip and rolldice

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 #this is the token needed to link this bot code back to the discord app
 TOKEN = 'NDk5NTAzNTc3MzUwMjc1MDcz.Dp9O5w.TJ09ddv1mW3AqPfym5aLWJlh2eg'
@@ -18,12 +19,26 @@ async def on_message(message):
     if any(word in message.content.lower() for word in cussWords):
        await client.send_message(message.channel,'No Cussing!')
        await client.delete_message(message)
-    if '!stop' in message.content.lower():
-        await client.logout()
+    await client.process_commands(message)
+    #if '!stop' in message.content.lower():
+        #await client.logout()
 
+@client.command()
+async def stop():
+    await client.say('You kids be good now!')
+    await client.logout()
 
+@client.command()
+async def rollsix():
+    a_number = random.randint(1, 6)
+    await client.say('You rolled a: ')
+    await client.say(a_number)
 
-#Yushuo will work on the two commands !online & !offline, to prompt the bot
-#to go online or offline.  Can add other commands if you want
+@client.command()
+async def coinflip():
+    coin = ['heads', 'tails']
+    rand = random.choice(coin)
+    await client.say('You got ')
+    await client.say(rand)
 
 client.run(TOKEN)
